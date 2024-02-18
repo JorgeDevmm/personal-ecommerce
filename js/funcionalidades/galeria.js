8f03db310fe5c29d1bb73d00cbabb5a32da0af1d
JorgeDevmm <jorge29luis10@gmail.com>
2024-02-18 00:42:13 -0500

    Agregando spinner galeria

--- js/funcionalidades/galeria.js
+++ js/funcionalidades/galeria.js
@@ -1,33 +1,64 @@
-const cargarGaleria = (objGaleria) => {
+const cargarGaleria = async (objGaleria) => {
   const listaGaleria = document.querySelector(`.galeria-listado`);
 
-  objGaleria.forEach((element) => {
+  limpiarHTML();
+
+  for (const element of objGaleria) {
     const liGaleria = document.createElement('li');
     const imgGaleria = document.createElement('img');
 
     // Verificar si existe el archivo con extensión .jpg
     const rutaJPG = `${element.src}`;
-    if (fileExists( rutaJPG)) {
+    const exists = await fileExistsAsync(rutaJPG);
+    if (exists) {
       imgGaleria.src = rutaJPG;
     } else {
       // Si no existe, asumir que existe con la extensión .jpeg
-
       imgGaleria.src = `${element.src}`;
     }
 
     // Establecer el atributo 'loading' en 'lazy'
     imgGaleria.setAttribute('loading', 'lazy');
 
+    // Agregar el spinner a cada li
+    const spinnerElement = spinner();
+    liGaleria.appendChild(spinnerElement);
+
+    // Escuchar el evento load de la imagen
+    imgGaleria.addEventListener('load', () => {
+      // Cuando la imagen carga, eliminar el spinner
+      liGaleria.removeChild(spinnerElement);
+    });
+
     liGaleria.append(imgGaleria);
     listaGaleria.append(liGaleria);
-  });
+  }
 
   // Función para verificar si un archivo existe
-  function fileExists(url) {
-    const http = new XMLHttpRequest();
-    http.open('HEAD', url, false);
-    http.send();
-    return http.status !== 404;
+  async function fileExistsAsync(url) {
+    try {
+      const response = await fetch(url, { method: 'HEAD' });
+      return response.ok;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  function limpiarHTML() {
+    // iterar si se encuentra un elemento en resultado
+    while (listaGaleria.firstChild) {
+      if (listaGaleria.firstChild) {
+        listaGaleria.removeChild(listaGaleria.firstChild);
+      }
+    }
+  }
+
+  // Función para crear y devolver un nuevo elemento de spinner
+  function spinner() {
+    const spinner = document.createElement('DIV');
+    // agrego clase de estilo spinner y para centrar x y y
+    spinner.classList.add('loader');
+    return spinner;
   }
 };
 
